37e4e918881e25722e958405f66501fed513b467
briankei <keibrianc@gmail.com>
2026-03-22 23:35:52 +0800

    Rename cache to smart-attendance

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 BrianKei
 // All rights reserved. See LICENSE for terms.
-const CACHE_NAME = 'af5122-attendance-v6.1';
+const CACHE_NAME = 'smart-attendance-v6.1';
 const ASSETS = [
   '/',
   '/index.html',
