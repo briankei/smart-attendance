8fa306051e5f2c188f54effa7b5734377d52005b
briankei <keibrianc@gmail.com>
2026-03-23 22:12:00 +0800

    v7.0 - GoatCounter analytics

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 BrianKei
 // All rights reserved. See LICENSE for terms.
-const CACHE_NAME = 'smart-attendance-v6.9';
+const CACHE_NAME = 'smart-attendance-v7.0';
 const ASSETS = [
   '/',
   '/index.html',
