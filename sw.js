ce78128ae06d3fad0c16680db2a3b1b8dddc3ec5
briankei <keibrianc@gmail.com>
2026-03-23 15:41:27 +0800

    v6.8 - Games: Random Groups, Random Order, Countdown Timer

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 BrianKei
 // All rights reserved. See LICENSE for terms.
-const CACHE_NAME = 'smart-attendance-v6.7';
+const CACHE_NAME = 'smart-attendance-v6.8';
 const ASSETS = [
   '/',
   '/index.html',
