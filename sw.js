755a2322e604f9842bcf460a7f3014146ae6d271
briankei <keibrianc@gmail.com>
2026-03-25 16:01:52 +0800

    v7.5 - Double confirm before restore, warn to export first

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 BrianKei
 // All rights reserved. See LICENSE for terms.
-const CACHE_NAME = 'smart-attendance-v7.4';
+const CACHE_NAME = 'smart-attendance-v7.5';
 const ASSETS = [
   '/',
   '/index.html',
